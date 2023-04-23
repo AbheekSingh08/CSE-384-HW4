f054824c6384e2d0c8343601909e01aa6ce866ce
AbheekSingh08 <130622441+AbheekSingh08@users.noreply.github.com>
2023-04-22 23:31:46 -0400

    Update number6.c

--- number6.c
+++ number6.c
@@ -4,11 +4,7 @@
 int main() {
     int ip_int = -1;
     struct in_addr ip_addr;
-
-    // Assign the integer value to s_addr
     ip_addr.s_addr = (in_addr_t)ip_int;
-
-    // Print the IP address string directly using printf and inet_ntoa
     printf("IP address: %s\n", inet_ntoa(ip_addr));
 
     return 0;
