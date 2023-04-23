3dddab75470b10c88122500cdf80ba3250d34e6f
AbheekSingh08 <130622441+AbheekSingh08@users.noreply.github.com>
2023-04-22 20:54:23 -0400

    Create number7.c

---
+++ number7.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+const char *process_ip_address(const char *ip_address_string, uint32_t *ip_address_integer) {
+    struct in_addr in_address;
+    int result = inet_pton(AF_INET, ip_address_string, &in_address);
+
+    if (result > 0) {
+        *ip_address_integer = ntohl(in_address.s_addr);
+        return "The integer value of the IP address %s is %u\n";
+    } else {
+        return "Error converting IP address: %s\n";
+    }
+}
+
+int main() {
+    const char *ip_address_string = "225.2.0.0";
+    uint32_t ip_address_integer;
+    const char *format_string;
+
+    format_string = process_ip_address(ip_address_string, &ip_address_integer);
+    printf(format_string, ip_address_string, ip_address_integer);
+
+    return 0;
+}
